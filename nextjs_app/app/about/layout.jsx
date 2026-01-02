ec00b0d9a6a97781bc9cd086c8d5a96afa9ce1e7
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-03 00:01:18 +0530

    Nested Routes & Layouts

---
+++ nextjs_app/app/about/layout.jsx
@@ -0,0 +1,15 @@
+//! nested layout of about section
+const AboutLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <h1>About Layout Page 😁</h1>
+        {/* Layout UI */}
+        {/* Place children where you want to render a page or nested layout */}
+        <main>{children}</main>
+      </body>
+    </html>
+  );
+};
+
+export default AboutLayout;
---
+++ nextjs_app/app/about/team/page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+//! this is a nested route of about section
+const TeamPage = () => {
+  return <div>TeamPage</div>;
+};
+
+export default TeamPage;
