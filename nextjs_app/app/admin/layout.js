7de5f2f236f36fb2acc976e7827703eeaf9303a7
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-04 16:28:59 +0530

    Route groups

---
+++ nextjs_app/app/admin/layout.js
@@ -0,0 +1,11 @@
+import "../globals.css";
+
+const AdminLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+};
+
+export default AdminLayout;
