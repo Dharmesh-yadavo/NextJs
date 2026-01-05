58d9ab305421377b132e7623672ee4edce4dd327
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-05 17:15:19 +0530

    Dynamic routing in clientComp

--- nextjs_app/app/(users)/users/[userName]/posts/[postId]/page.jsx
+++ nextjs_app/app/(users)/users/[userName]/posts/[postId]/page.jsx
@@ -1,14 +1,18 @@
-const postIdPage = async (props) => {
-  const user = await props.params;
+"use client";
+
+import { use } from "react";
+
+const PostIdPage = (props) => {
+  const user = use(props.params);
 
   //   console.log(user);
 
   return (
     <div className="mt-10 ml-14 flex flex-col gap-2 text-purple-300 ">
-      <h1>userNAme: {user.userName}</h1>
+      <h1>userName: {user.userName}</h1>
       <h2>postId: {user.postId}</h2>
     </div>
   );
 };
 
-export default postIdPage;
+export default PostIdPage;
