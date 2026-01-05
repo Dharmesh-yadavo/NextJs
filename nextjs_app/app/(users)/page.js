132d6e64941e432bf1848752bd9e64e9300b9b9b
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-05 10:53:36 +0530

    font components

--- nextjs_app/app/(users)/page.js
+++ nextjs_app/app/(users)/page.js
@@ -1,7 +1,25 @@
+import { Delius } from "next/font/google";
+import { Meie_Script } from "next/font/google";
+
+const delius = Delius({
+  subsets: ["latin"],
+  weight: "400",
+});
+
+const meieScript = Meie_Script({
+  subsets: ["latin"],
+  weight: "400",
+});
+
 const Home = () => {
   return (
-    <div className="min-h-screen flex items-center justify-center">
-      <h1 className="text-3xl">Hello, Welcome to NextJs 😁 </h1>
+    <div
+      className={`min-h-screen flex flex-col gap-6 items-center justify-center text-4xl `}
+    >
+      <h1 className={`${delius.className} text-orange-200`}>
+        Hello, Welcome to NextJs 😁
+      </h1>
+      <p className={meieScript.className}>Dharma Bhai</p>
     </div>
   );
 };
