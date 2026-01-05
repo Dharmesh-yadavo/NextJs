f4ffc9a3594dc24f0a50475d183b5a29017e7c28
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-05 11:14:56 +0530

    applying multiple fonts

--- nextjs_app/app/(users)/page.js
+++ nextjs_app/app/(users)/page.js
@@ -1,25 +1,10 @@
-import { Delius } from "next/font/google";
-import { Meie_Script } from "next/font/google";
-
-const delius = Delius({
-  subsets: ["latin"],
-  weight: "400",
-});
-
-const meieScript = Meie_Script({
-  subsets: ["latin"],
-  weight: "400",
-});
-
 const Home = () => {
   return (
     <div
       className={`min-h-screen flex flex-col gap-6 items-center justify-center text-4xl `}
     >
-      <h1 className={`${delius.className} text-orange-200`}>
-        Hello, Welcome to NextJs 😁
-      </h1>
-      <p className={meieScript.className}>Dharma Bhai</p>
+      <h1 className="font-delius">Hello, Welcome to NextJs 😁</h1>
+      <p className="font-meie">Dharma Bhai</p>
     </div>
   );
 };
--- nextjs_app/app/layout.js
+++ nextjs_app/app/layout.js
@@ -1,9 +1,25 @@
 import "./globals.css";
 
+import { Delius, Meie_Script } from "next/font/google";
+
+const delius = Delius({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-delius",
+});
+
+const meieScript = Meie_Script({
+  subsets: ["latin"],
+  weight: "400",
+  variable: "--font-meie-script",
+});
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${delius.variable} ${meieScript.variable}`}>
+        {children}
+      </body>
     </html>
   );
 }
