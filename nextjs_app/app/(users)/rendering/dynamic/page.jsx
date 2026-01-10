ceaf00b9153cae40d4a1b901005e904c7304dcc1
Dharmesh Yadav <166201809+Dharmesh-yadavo@users.noreply.github.com>
2026-01-10 09:54:36 +0530

    React Cache in NextJs

--- nextjs_app/app/(users)/rendering/dynamic/page.jsx
+++ nextjs_app/app/(users)/rendering/dynamic/page.jsx
@@ -1,12 +1,25 @@
 import { db } from "@/config/db.jsx";
+import { cache } from "react";
+
+export const dynamic = "force-dynamic";
 
 const DynamicPage = async () => {
-  const [doctors] = await db.execute("select * from doctors");
-  console.log("fetching doctors");
+  const doctors = await getAllDoctors();
 
   return (
     <>
       <p>Total Doctors: {doctors.length}</p>
+      <DoctorLists doctors={doctors} />
+    </>
+  );
+};
+
+export default DynamicPage;
+
+const DoctorLists = async () => {
+  const doctors = await getAllDoctors();
+  return (
+    <>
       <ul>
         {doctors.map((doctor) => {
           return <li key={doctor.doctor_id}> {doctor.first_name} </li>;
@@ -16,4 +29,9 @@ const DynamicPage = async () => {
   );
 };
 
-export default DynamicPage;
+//! React Cache
+const getAllDoctors = cache(async () => {
+  const [doctors] = await db.execute("select * from doctors");
+  // console.log("fetching doctors");
+  return doctors;
+});
